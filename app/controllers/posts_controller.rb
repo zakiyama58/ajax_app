b05260ac8536e9606c222a88259b6ec58cffbc2e
zakiyama58 <t.t.t.t.ty.1016.66@gmail.com>
2021-03-07 23:27:20 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all.order(id: "DESC")
   end
@@ -11,13 +10,13 @@ class PostsController < ApplicationController
 
   def checked
     post = Post.find(params[:id])
-    if post.checked
+    if post.checked 
       post.update(checked: false)
     else
       post.update(checked: true)
     end
+
     item = Post.find(params[:id])
     render json: { post: item }
   end
-
 end
